8d72230d7c578365c0b67521882d0c75f656f71b
bonnie <owu.owu.666@gmail.com>
2025-05-10 14:11:47 -0400

    serializer updates

--- main_app/models.py
+++ main_app/models.py
@@ -6,7 +6,6 @@ from django.contrib.auth.models import User
 
 class Cohort (models.Model):
     name = models.CharField(max_length=50)
-    # created_at = models.TimeField()
     
     def __str__(self):
         return self.name
