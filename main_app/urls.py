93b1961d917357ef816140c03faf4341c863bcd7
bonnie <owu.owu.666@gmail.com>
2025-05-10 11:50:05 -0400

    soem shit

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
-from .views import Home
+from .views import (Home, 
+                    ListProfile, 
+                    ProfileDetail,
+                    PointEventEdit,
+                    )
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
--- raul_point_collector/settings.py
+++ raul_point_collector/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'pillow',
     'rest_framework',
   	'main_app',
 ]
