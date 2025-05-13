eeba86a24562481953902f501c1cccd03ad0a32d
StellaTeuscher <stella.teuscher@gmail.com>
2025-05-13 14:52:40 -0400

    added verify user

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
-from .views import Home, CreateUser, LoginUser, ProfilesListView, ProfileDetail, PointEventUpdate
+from .views import Home, CreateUser, LoginUser, VerifyUser, ProfilesListView, ProfileDetail, PointEventUpdate
 
 urlpatterns = [
   path('', Home.as_view(), name='home'),
   path('register/', CreateUser.as_view(), name='register'),
   path('login/', LoginUser.as_view(), name='login'),
+  path('token/refresh', VerifyUser.as_view(), name="token_refresh"),
   path('profiles/', ProfilesListView.as_view(), name='profiles-list-create'), 
   path('profiles/<int:id>', ProfileDetail.as_view(), name="profile-detail"),
   path('profiles/<int:id>/points',PointEventUpdate.as_view(), name="update-points"),
